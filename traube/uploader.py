ce6025225e9c492385459a1528fe10196e334d32
stefan <dev@2904.cc>
2016-08-23 01:03:32 +0200

    implemented prototype uploader

---
+++ traube/uploader.py
@@ -0,0 +1,63 @@
+#!/usr/bin/env python
+
+__author__ = "stefan"
+
+import os
+
+import click
+
+from config import loadConfig
+from index import Index
+from crypto import Crypto
+
+
+class Uploader():
+    def __init__(self, remote, idx, crypto):
+        self.remote = remote
+        self.idx = idx
+        self.crypto = crypto
+        print('# TODO: create temporary dir $tmp')
+        self.tmp = '/tmp'
+
+    def upload(self, name, dir, keys):
+        print('   %s: uploading %s ...' % (name, dir))
+        for file in os.listdir(dir):
+            path = dir + '/' + file
+            print('    uploading %s' % path)
+            if self.idx.isUploaded(name, file):
+                print('      already in index. skipping ...')
+            else:
+                alias = self.crypto.getRandomBits()
+                self.__doUpload(alias, path)
+                self.idx.add(name, alias, file)
+
+    def __doUpload(self, alias, path):
+        print('# TODO: encrypt file $path to $tmp/$alias')
+        print('# TODO: upload $tmp/$alias to $self.remote')
+
+
+@click.command()
+@click.option('--config', help='Location of your traube.cfg', required=True)
+def main(config):
+    print(' loading config from %s' % config)
+    base_path, idx_path, remote, key, keys, sources = loadConfig(config)
+
+    print(' initializing crypto')
+    crypto = Crypto(key, keys, base_path)
+
+    print(' loading index from %s' % idx_path)
+    idx = Index(idx_path, crypto)
+
+    upl = Uploader(remote, idx, crypto)
+
+    for name, data in sources.items():
+        dir = base_path + data['dir']
+        keys = data['keys']
+        upl.upload(name, dir, keys)
+
+    idx.save()
+
+
+if __name__ == '__main__':
+    print('Welcome to traube!')
+    main()
